701fb7563fa7809765084b6222c1bbb60165f88d
Sho Nakatani <lay.sakura@gmail.com>
2021-03-22 13:42:36 +0900

    rm unused channel parameter

--- actfw_jetson/camera.py
+++ actfw_jetson/camera.py
@@ -102,7 +102,6 @@ def _extract_buffer(sample):
     buffer = sample.get_buffer()  # Gst.Buffer
     caps_format = sample.get_caps().get_structure(0)  # Gst.Structure
     w, h = caps_format.get_value('width'), caps_format.get_value('height')
-    c = 4  # RGBA
 
     buffer_size = buffer.get_size()
     return Image.frombuffer('RGBA', (w, h), buffer.extract_dup(0, buffer_size))
